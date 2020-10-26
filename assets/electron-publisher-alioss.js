71d451a4ad88f6cb063c90c662b6f2b6a4048854
Meng Li <Mengli619@github.com>
2020-10-26 13:00:02 +0800

    update electron publsher

--- assets/electron-publisher-alioss.js
+++ assets/electron-publisher-alioss.js
@@ -4,27 +4,17 @@ const { HttpPublisher } = require('electron-publish');
 const { basename, resolve } = require('path');
 
 class AliOssPublisher extends HttpPublisher {
-    providerName = 'alioss';
-    useSafeName = true;
-    client;
-    context;
-    config;
-
     constructor(context, publishConfig, useSafeArtifactName) {
         super(context);
+        this.providerName = 'alioss';
+        this.useSafeName = true;
         this.useSafeName = useSafeArtifactName || true;
         let config = publishConfig;
         if (publishConfig.localConfig) {
             const localConfig = require(resolve(this.context.packager.appDir, config.localConfig));
-            config = {
-                ...config,
-                ...localConfig
-            }
+            config = Object.assign({}, config, localConfig);
         }
-        this.config = {
-            resumable: true,
-            ...config
-        };
+        this.config = Object.assign({ resumable: true }, config);
         this.client = new OSS({
             region: config.region,
             accessKeyId: config.accessKeyId,
@@ -33,21 +23,19 @@ class AliOssPublisher extends HttpPublisher {
             timeout: config.timeout || 60000,
         });
     }
-
     async upload(task) {
         const fileName = (this.useSafeName ? task.safeArtifactName : null) || basename(task.file);
         const os = task.packager['platform'].buildConfigurationKey || task.packager['platform'].name;
         let archName = Arch[Arch.x64];
-        if(!task.arch && task.arch !== 0){
-            if(task.packager['platform'].nodeName.indexOf('32') >= 0){
-                archName = Arch[Arch.ia32]
+        if (!task.arch && task.arch !== 0) {
+            if (task.packager['platform'].nodeName.indexOf('32') >= 0) {
+                archName = Arch[Arch.ia32];
             }
-        } else{
-            archName = Arch[task.arch]
+        } else {
+            archName = Arch[task.arch];
         }
         await this.doUpload(fileName, task.file, archName, os);
     }
-
     async doUpload(fileName, filePath, archName, os) {
         const config = this.config;
         const appInfo = this.context.packager.appInfo;
@@ -66,20 +54,19 @@ class AliOssPublisher extends HttpPublisher {
             try {
                 for (let i = 0; i < (resumable ? maxResume : 1); i++) {
                     // try to resume the upload
-                    console.log(`${uploadName}: uploading...🕑 `)
+                    console.log(`${uploadName}: uploading...🕑 `);
                     const result = await this.client.multipartUpload(uploadName, filePath, {
                         progress: async (percentage, cpt) => {
                             checkpoint = cpt;
                             if (this.config.verbose && cpt) {
-                                console.log(`${uploadName}: ${cpt.doneParts.length}\/${Math.ceil(cpt.fileSize / cpt.partSize)}(${(percentage * 100).toFixed(2)}%)`)
+                                console.log(`${uploadName}: ${cpt.doneParts.length}\/${Math.ceil(cpt.fileSize / cpt.partSize)}(${(percentage * 100).toFixed(2)}%)`);
                             }
                         },
                         checkpoint: checkpoint,
-                        meta: {
-                        }
+                        meta: {}
                     });
                     resolve(result);
-                    console.log(`${uploadName}: upload success...✅ `)
+                    console.log(`${uploadName}: upload success...✅ `);
                     break; // break if the upload success;
                 }
             } catch (e) {
